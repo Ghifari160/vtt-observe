321a44a90836cce4626735f2413ee79f17ed9ee8
GHIFARI160 <ghifari@ghifari160.com>
2022-12-13 00:14:04 -0600

    Add scripts
    Simplify the dev process with scripts. While most scripts are simple NPM
    scripts, some must be implemented in JS. Instead of implementing them in
    JS, implement them in TS for type safety, then provide an NPM script
    configure. That script should be run before running any other script.

---
+++ scripts/manifest/src/index.ts
@@ -0,0 +1,60 @@
+import path from "path";
+import fs from "fs";
+import process from "process";
+
+const app = "VTT Observe";
+
+const pkgManName = "package.json";
+const extManName = "manifest.json";
+const buildDir = "out";
+
+const perms = [ "activeTab", "scripting" ];
+const vttWhitelist = [ "https://app.roll20.net/editor/*" ];
+const scripts = [ "content/content.js" ];
+
+main();
+
+function main():void {
+    const pkgManPath = path.resolve(process.cwd(), pkgManName);
+    const outManPath = path.resolve(process.cwd(), "out", extManName);
+
+    stepGenManifest(pkgManPath, outManPath);
+}
+
+function stepParseManifest(manPath:string):any {
+    let manStr = fs.readFileSync(manPath, "utf8");
+
+    return JSON.parse(manStr)
+}
+
+function stepGetContentScripts():any {
+    return [{
+        js: scripts,
+        matches: vttWhitelist,
+    }];
+}
+
+function stepGenManifest(pkgManPath:string, extManOut:string) {
+    let pkg = stepParseManifest(pkgManPath);
+
+    let ext = {
+        // required fields
+        manifest_version: 3,
+        name: app,
+        version: pkg.version,
+
+        // recommended fields
+        // default_locale: "en",
+        description: pkg.description,
+
+        // optional fields
+        author: pkg.author,
+        content_scripts: stepGetContentScripts(),
+        homepage_url: pkg.homepage,
+        permissions: perms,
+    };
+
+    let extStr = JSON.stringify(ext);
+
+    fs.writeFileSync(extManOut, extStr);
+}
