a9d98a5669bcf86bd440d1f9dfc9c71b1b7240de
GHIFARI160 <ghifari@ghifari160.com>
2022-12-26 22:40:02 -0600

    Refactor webpack config
    Move config into multicompiler setup. This prepares the repo for
    building pages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
 const path = require("path");
 
-module.exports = {
-    entry: {
-        background: "./background/index.ts",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
+const webpack = require("webpack");
+
+const outputPath = path.resolve(__dirname, "out");
+
+module.exports = [
+    {
+        name: "extension",
+        entry: {
+            background: path.resolve(__dirname, "background", "index.ts"),
+        },
+        output: {
+            filename: "[name].js",
+            path: outputPath,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [ ".tsx", ".ts", ".js" ],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                ENV: JSON.stringify("extension"),
+            }),
         ],
     },
-    resolve: {
-        extensions: [ ".tsx", ".ts", ".js" ],
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "out")
-    },
-};
+];
